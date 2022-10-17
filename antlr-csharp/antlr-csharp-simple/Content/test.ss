bb96aa5c232de10aeb84285d7c32db0f77b2a8c7
Janos Kulcsar <janos.kulcsar@mcs.ch>
2022-10-17 11:56:52 +0200

    Finishing up the Simple script parser - with (very) simple Visitor actions implementations

--- antlr-csharp/antlr-csharp-simple/Content/test.ss
+++ antlr-csharp/antlr-csharp-simple/Content/test.ss
@@ -7,11 +7,14 @@ while i < 8
 {
     Write("i isn't big enough - " + i + " - so let's make it bigger!");
     i = i + 1 * 4;
-    
+
+    concat_name = 'John' + i +  'Whatever';    
 }
 else
 {
     Write("i was plenty big to begin with.");
 }
+
+j = i;
 
 Write("i is now " + i + " and name is " + name);
--- antlr-csharp/antlr-csharp-simple/MySimpleVisitor.cs
+++ antlr-csharp/antlr-csharp-simple/MySimpleVisitor.cs
@@ -34,7 +34,163 @@ public class MySimpleVisitor : SimpleBaseVisitor<object?>
         {
             return float.Parse(aFloat.GetText());
         }
+        if(context.BOOL() is {} aBool)
+        {
+            return aBool.GetText() == "true";
+        }
+        if(context.NULL() is {} aNull)
+        {
+            return null;
+        }
+
+        throw new NotImplementedException();
+    }
+
+    public override object? VisitIdentifierExpression(SimpleParser.IdentifierExpressionContext context)
+    {
+        var variableName = context.IDENTIFIER().GetText();
+
+        if (!_variables.ContainsKey(variableName))
+        {
+           throw new Exception($"Variable {variableName} not defined.");
+        }
         
-        return base.VisitConstant(context);
+        return _variables[variableName];
+    }
+
+    public override object? VisitAdditiveExpression(SimpleParser.AdditiveExpressionContext context)
+    {
+        var left = Visit(context.expression(0));
+        var right = Visit(context.expression(1));
+
+        var op = context.addOp().GetText();
+        return op switch
+        {
+            "+" => Add(left, right),
+            "-" => Substract(left, right),
+            _ => throw new NotImplementedException()
+        };
+        
+        throw new NotImplementedException();
+    }
+    
+    public object? Add(object? left, object? right)
+    {
+        if (left is int leftInt && right is int rightInt)
+        {
+            return leftInt + rightInt;
+        }
+        if (left is float leftFloat && right is float rightFloat)
+        {
+            return leftFloat + rightFloat;
+        }
+        if (left is string || right is string)
+        {
+            return $"{left}{right}";
+        }
+
+        throw new Exception($"Cannot add values of type {left?.GetType()} and {right?.GetType()}.");
+    }
+    
+    public object? Substract(object? left, object? right)
+    {
+        if (left is int leftInt && right is int rightInt)
+        {
+            return leftInt - rightInt;
+        }
+        if (left is float leftFloat && right is float rightFloat)
+        {
+            return leftFloat - rightFloat;
+        }
+
+        throw new NotImplementedException();
+    }
+
+    public override object? VisitWhileBlock(SimpleParser.WhileBlockContext context)
+    {
+        Func<object?, bool> condition = context.WHILE().GetText() == "while"
+        ? IsTrue
+        : IsFalse;
+        
+        // implements the purposefully defined strange  while...else loop
+        if(condition(Visit(context.expression())))
+        {
+            do
+            {
+                Visit(context.block());
+            } while(condition(Visit(context.expression())));
+        }
+        else
+        {
+            Visit(context.block());
+        }
+
+        return null;
+    }
+
+    public override object? VisitComparisonExpression(SimpleParser.ComparisonExpressionContext context)
+    {
+
+        var left = Visit(context.expression(0));
+        var right = Visit(context.expression(1));
+
+        var op = context.compareOp().GetText();
+        return op switch
+        {
+            "==" => Equals(left, right),
+            "!=" => !Equals(left, right),
+            "<" => LessThan(left, right),
+            //">" => GreaterThan(left, right),
+            //"<=" => LessThanOrEqual(left, right),
+            //">=" => GreaterThanOrEqual(left, right),
+            _ => throw new NotImplementedException()
+        };
+    }
+
+    private object? LessThan(object? left, object? right)
+    {
+
+        if (left is int leftInt && right is int rightInt)
+        {
+            return leftInt < rightInt;
+        }
+
+        if (left is float leftFloat && right is float rightFloat)
+        {
+            return leftFloat < rightFloat;
+        }
+        
+        throw new NotImplementedException();
+    }
+
+    private bool IsTrue(object? value)
+    {
+        if (value is bool boolValue)
+        {
+            return boolValue;
+        }
+        if (value is int intValue)
+        {
+            return intValue != 0;
+        }
+        if (value is float floatValue)
+        {
+            return floatValue != 0;
+        }
+        if (value is string stringValue)
+        {
+            return stringValue != "";
+        }
+        if (value is null)
+        {
+            return false;
+        }
+
+        throw new Exception($"Cannot convert value of type {value?.GetType()} to boolean.");
+    }
+    
+    public bool IsFalse(object? value)
+    {
+        return !IsTrue(value);
     }
 }
